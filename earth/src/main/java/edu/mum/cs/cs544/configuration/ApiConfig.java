767b304017578ffaa3b972d025a3be8a0ce83260
samhela <saminium@gmail.com>
2017-02-06 01:55:44 -0600

    Jersey Configured
    Web service API is configured

---
+++ earth/src/main/java/edu/mum/cs/cs544/configuration/ApiConfig.java
@@ -0,0 +1,20 @@
+package edu.mum.cs.cs544.configuration;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by samue on 2/6/2017.
+ */
+@ApplicationPath("/api")
+public class ApiConfig extends Application {
+    @Override
+    public Map<String, Object> getProperties() {
+        System.out.println("*********************API method called***********************");
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put("jersey.config.server.provider.packages", "edu.mum.cs.cs544");
+        return properties;
+    }
+}
---
+++ earth/src/main/java/edu/mum/cs/cs544/ws/CourseApi.java
@@ -0,0 +1,34 @@
+package edu.mum.cs.cs544.ws;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.*;
+
+/**
+ * Created by samue on 2/6/2017.
+ */
+@Path("/course")
+public class CourseApi {
+
+
+    Map<String, String>  member1 = new HashMap<String, String>() ;
+    Map<String, String>  member2 = new HashMap<String, String>() ;
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public  List<Map<String, String>> getAllCourses(){
+        List<Map<String, String>> listOfProjectTeamMembers = new ArrayList<Map<String, String>>();
+        member1.put("name", "Team 1");
+        member1.put("task", "Developer");
+
+        member2.put("name", "Team 2");
+        member2.put("task", "Tester");
+
+        listOfProjectTeamMembers.addAll(Arrays.asList(member1, member2));
+
+        return listOfProjectTeamMembers;
+    }
+}
