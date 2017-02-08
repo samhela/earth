8f0b96e695d17e09f1121e657ade5514e436962d
bimal <bimalparajulise@gmail.com>
2017-02-07 21:22:06 -0600

    faculty daos

---
+++ earth/src/main/java/edu/mum/cs/cs544/service/FacultyService.java
@@ -0,0 +1,37 @@
+package edu.mum.cs.cs544.service;
+
+import java.util.List;
+
+import edu.mum.cs.cs544.model.AttendanceRecord;
+import edu.mum.cs.cs544.model.Course;
+import edu.mum.cs.cs544.model.CourseOffering;
+import edu.mum.cs.cs544.model.Location;
+import edu.mum.cs.cs544.model.Session;
+import edu.mum.cs.cs544.model.Student;
+import edu.mum.cs.cs544.model.Timeslot;
+
+
+public interface FacultyService {
+
+	Student findByStudentId(int id);
+
+	Student findByFirstname(String firstname);
+	
+	Student findByLastname(String lastname);
+	
+	List<Student> findAll();
+	
+	Course viewByCouseId(String courseId);
+	
+	List<CourseOffering> findAllCourseOffering(String courseId);
+	
+	List<Session> viewListOfSession(CourseOffering courseOfferingId);
+	
+	List<Location> readListOfLocation(CourseOffering courseOfferingId);
+	
+	List<Timeslot> readListOfTimeSlot();
+	
+	List<AttendanceRecord> findStudentAttendanceRecords(Student student);
+	
+	
+}
