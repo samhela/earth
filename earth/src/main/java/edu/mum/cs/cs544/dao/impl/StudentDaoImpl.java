1201eadf0f74ee96d92fd1bfa3f94afe68e69722
samhela <saminium@gmail.com>
2017-02-11 15:03:20 -0600

    Final Comits

--- earth/src/main/java/edu/mum/cs/cs544/dao/impl/StudentDaoImpl.java
+++ earth/src/main/java/edu/mum/cs/cs544/dao/impl/StudentDaoImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository("studentDao")
+@Repository
 public class StudentDaoImpl  extends  AbstractDao<Integer, Student> implements StudentDao {
 
     //harcoding
